7b4820b885dae8e0996e1f44152b789b9d74e420
Muhammad Zaki <muhammadzaki32995@gmail.com>
2020-05-20 17:16:39 +0700

    menambahkan input username,email,password

--- coba.php
+++ coba.php
@@ -1,1 +1,25 @@
-	<button type="Submit" name="name" </button>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <label>
+    Username :
+    <input type="text" name="username">
+  </label>
+  <label>
+    Password :
+    <input type="password" name="password">
+  </label>
+  <label>
+    Email :
+    <input type="email" name="email">
+  </label>
+</body>
+
+</html>

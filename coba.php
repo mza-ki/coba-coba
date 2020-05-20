d11331141ca877c084df1a166bdbd04975e79de9
Muhammad Zaki <muhammadzaki32995@gmail.com>
2020-05-20 17:13:03 +0700

    memperbaiki file coba.php

--- coba.php
+++ coba.php
@@ -1,16 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=u, initial-scale=1.0">
-  <title>Document</title>
-</head>
-
-<body>
-  <ul>
-    <li></li>
-  </ul>
-</body>
-
-</html>
+	<button type="Submit" name="name" </button>
